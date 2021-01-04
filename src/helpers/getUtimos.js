7b16d6801de830cf1176903c72f46e5c49762a2c
juan <jua1231@hotmail.com>
2021-01-04 14:52:16 -0500

    ajustes url api

--- src/helpers/getUtimos.js
+++ src/helpers/getUtimos.js
@@ -1,7 +1,5 @@
 export const getUltimos = async () => {
-
-  const resp = await fetch('https://tickets-socket-app.herokuapp.com')
+  const resp = await fetch('https://tickets-socket-app.herokuapp.com/ultimos')
   const data = await resp.json();
   return data.ultimos;
-
 }
