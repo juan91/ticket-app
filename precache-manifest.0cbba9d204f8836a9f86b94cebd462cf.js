25d524908e8a2d61e93aa6a0c38af56a8e308a4a
juan <jua1231@hotmail.com>
2021-01-04 12:36:45 -0500

    Updates

---
+++ precache-manifest.0cbba9d204f8836a9f86b94cebd462cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4b9de6f605c140e37b133d99d438c0c",
+    "url": "/juan91/ticket-app/index.html"
+  },
+  {
+    "revision": "0db69bba32255844d1f6",
+    "url": "/juan91/ticket-app/static/css/2.e10332d9.chunk.css"
+  },
+  {
+    "revision": "e0928b8c10dd5aec9fe5",
+    "url": "/juan91/ticket-app/static/css/main.dff3c0e4.chunk.css"
+  },
+  {
+    "revision": "0db69bba32255844d1f6",
+    "url": "/juan91/ticket-app/static/js/2.a8b273d2.chunk.js"
+  },
+  {
+    "revision": "88cbcb55f867c7e879dbc5e47becbf3b",
+    "url": "/juan91/ticket-app/static/js/2.a8b273d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0928b8c10dd5aec9fe5",
+    "url": "/juan91/ticket-app/static/js/main.0b43ef33.chunk.js"
+  },
+  {
+    "revision": "623ad7b10f043bcdcf87",
+    "url": "/juan91/ticket-app/static/js/runtime-main.d19b4e76.js"
+  }
+]);
